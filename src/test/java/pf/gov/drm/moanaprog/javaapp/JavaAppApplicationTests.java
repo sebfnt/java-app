6a8517653e46dd9b1d424cc2e45524bb72495ac5
Sebastien Font <sebastien@tealforge.com>
2024-06-24 10:18:17 +1100

    génération projet java 21

---
+++ src/test/java/pf/gov/drm/moanaprog/javaapp/JavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pf.gov.drm.moanaprog.javaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
